17edef281db41b507e4ab2e91226de55af41fcb1
LmaAl-Johare1 <lmaref.j@gmail.com>
2023-02-25 21:51:49 +0200

    skelton for project

---
+++ Calculater/Calculater/Calculater/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Calculater.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Calculater.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Calculater/Calculater/Calculater/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Calculater.Models;
+using Calculater.ViewModels;
+using Calculater.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculater.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
